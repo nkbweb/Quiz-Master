8934b537d1277d6eeb899856b4e709bf1aea45e6
Nishchay Kumar Bhardwaj <168272378+nkbweb@users.noreply.github.com>
2025-03-09 10:50:11 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
